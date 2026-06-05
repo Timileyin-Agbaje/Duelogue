b4aca1039a4cc1d9995981ab754d214b8c633f84
Timileyin Agbaje <timi.agbaje.mail@gmail.com>
2026-06-05 08:13:22 +0000

    feat: add Hero component for landing page with title, subtitle, and buttons

---
+++ components/ui/hero.tsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+
+export default function Hero() {
+  return(
+    <section className="flex flex-col items-center">
+        <div>
+            <h1 className="text-5xl font-extrabold md:text-7xl tracking-tight leading-tight mt-20" >Discover Better Answers</h1>
+            <h2 className="text-xl md:text-2xl font-medium text-gray-600 max-w-3xl mx-auto text-center" >See both sides of any topic through clear comparision</h2>
+            <span className="flex justify-center gap-2 mt-4"><Button className="cursor-pointer">See pros & cons</Button><Button className="cursor-pointer" variant="secondary">Browse Examples</Button></span>
+            <Image src="/images/images.png" alt="Hero Image" width={800} height={600} className="w-full h-auto mt-8 object-contain"/>
+        </div>
+    </section>
+  )
+
+}
