eaf224d3d1ecc7ee34e3820b803444a21470868a
Timileyin Agbaje <timi.agbaje.mail@gmail.com>
2026-06-05 16:34:07 +0000

    feat: enhance Home component layout by adding Hero, Features, HowItWorks, PreFooter, and Footer components

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,30 @@
 import { Button } from "@/components/ui/button";
 import  Header  from "@/components/ui/header";
+import Hero from "@/components/ui/hero";
+import Features from "@/components/ui/features";
+import PreFooter from "@/components/preFooter";
+import Footer from "@/components/ui/footer";
+import HowItWorks from "@/components/ui/how-it-works";
+
+export const viewport = "width=device-width, initial-scale=1.0";
+
+const featuresData = [
+    {id:1, img: "/images/diagram-1.svg", alt: "Diagram of Unbaised Perspective", title: "Unbiased Perspective", Description: "Each topic is analyzed for\n pros and cons without\n hidden bias or influence."},
+    {id:2, img: "/images/diagram-2.svg", alt: "Diagram of Clear Points", title: "Clear Points", Description: "Quickly Understand key\n points with concise,\n well-formatted breakdowns." },
+    {id:3, img: "/images/diagram_3.svg", alt: "Diagram of Actionable Insights", title: "Actionable Insights", Description: "Gain practical next steps\n and real takeaways to\n inform your decisions."}
+]
 
 export default function Home() {
   return (
     <div>
         <Header />
+        <Hero    />
+        {featuresData.map(feature => (
+            <Features  key={feature.id} id={feature.id} img={feature.img} alt={feature.alt} title={feature.title} Description={feature.Description} />
+        ))}
+        <HowItWorks />
+        <PreFooter />
+        <Footer />
     </div>
   );
 }
