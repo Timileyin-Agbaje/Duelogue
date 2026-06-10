f9414800fbba7b239374fb7b4a4a17d44bd5ee63
Timileyin Agbaje <timi.agbaje.mail@gmail.com>
2026-06-10 12:58:05 +0000

    feat: simplify sidebar header layout by removing unused components

--- components/dashboard/sidebar/sidebarHeader.tsx
+++ components/dashboard/sidebar/sidebarHeader.tsx
@@ -1,12 +1,11 @@
 import Image from "next/image"
-import { PanelRight } from 'lucide-react'
+
 
 export default function sidebarHeader() {
     return(
         <div>
-            <header className="flex flex-row justify-between">
+            <header >
                 <Image src="/logos/duelogue-logo.svg" alt="Duelogue Logo" width={200} height={50} className="h-8 w-auto"/>
-                <PanelRight className="cursor-pointer" />
             </header>
         </div>
     )
