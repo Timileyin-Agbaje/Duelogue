ae6d5bb3bb356f6a7234d1006753e79593206743
Timileyin Agbaje <timi.agbaje.mail@gmail.com>
2026-06-10 12:58:21 +0000

    feat: enhance sidebar button layout with improved styling and spacing

--- components/dashboard/sidebar/sidebarItems.tsx
+++ components/dashboard/sidebar/sidebarItems.tsx
@@ -3,9 +3,9 @@ import { MessageCircle, History} from 'lucide-react'
 export default function sidebarItems() {
     return(
         <div>
-            <section className="flex flex-col gap-7 mt-10">
-                <button className="flex flex-row gap-2"><MessageCircle/>New chat</button>
-                <button className="flex flex-row gap-2"><History/>History</button>
+            <section className="flex flex-col gap-1 mt-10">
+                <button className=" hover:bg-gray-100  w-10/12 p-2 rounded-lg flex flex-row gap-1 items-center text-sm text-gray-800"><MessageCircle className='h-7 w-6'/><p>New chat</p></button>
+                <button className=" hover:bg-gray-100  w-10/12 p-2 rounded-lg flex flex-row gap-1 items-center text-sm  text-gray-800"><History className='h-7 w-6'/><p>History</p></button>
             </section>
         </div>
     )
