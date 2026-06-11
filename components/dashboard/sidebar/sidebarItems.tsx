cec8b89b67922add3dd90b0a326176db88c57c33
Timileyin Agbaje <timi.agbaje.mail@gmail.com>
2026-06-11 05:45:24 +0000

    feat: enhance sidebar items layout with dynamic spacing and visibility based on sidebar state

--- components/dashboard/sidebar/sidebarItems.tsx
+++ components/dashboard/sidebar/sidebarItems.tsx
@@ -1,11 +1,18 @@
 import { MessageCircle, History} from 'lucide-react'
+import { useSidebar } from "@/components/dashboard/context/SidebarContext"
 
 export default function sidebarItems() {
+    const { isCollapsed, toggle } = useSidebar();
     return(
         <div>
-            <section className="flex flex-col gap-1 mt-10">
-                <button className=" hover:bg-gray-100  w-10/12 p-2 rounded-lg flex flex-row gap-1 items-center text-sm text-gray-800"><MessageCircle className='h-7 w-6'/><p>New chat</p></button>
-                <button className=" hover:bg-gray-100  w-10/12 p-2 rounded-lg flex flex-row gap-1 items-center text-sm  text-gray-800"><History className='h-7 w-6'/><p>History</p></button>
+            <section className={`${isCollapsed ? "gap-10" : "gap-1"}flex flex-col mt-10`}>
+                <button className=" hover:bg-gray-100  w-10/12 p-2 rounded-lg flex flex-row gap-1 items-center text-sm text-gray-800">
+                <MessageCircle className="w-6 h-6 shrink-0"/>
+                <p className={`${isCollapsed ? "hidden" : "block"}`}>New chat</p>
+                </button>
+                <button className="hover:bg-gray-100  w-10/12 p-2 rounded-lg flex flex-row gap-1 items-center text-sm  text-gray-800">
+                    <History className="w-6 h-6 shrink-0"/> <p className={`${isCollapsed ? "hidden" : "block"}`} >History</p>
+                </button>
             </section>
         </div>
     )
