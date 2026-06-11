f1f1a396d423fbacdb4f1c0ffb427364e3d38d59
Timileyin Agbaje <timi.agbaje.mail@gmail.com>
2026-06-11 05:44:01 +0000

    feat: integrate sidebar state management into SidebarHeader component

--- components/dashboard/sidebar/sidebarHeader.tsx
+++ components/dashboard/sidebar/sidebarHeader.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image"
+import { useSidebar } from "@/components/dashboard/context/SidebarContext"
 
 
 export default function sidebarHeader() {
+     const { isCollapsed, toggle } = useSidebar();
     return(
         <div>
             <header >
-                <Image src="/logos/duelogue-logo.svg" alt="Duelogue Logo" width={200} height={50} className="h-8 w-auto"/>
+                <Image src="/logos/duelogue-logo.svg" alt="Duelogue Logo" width={200} height={50} className={`${isCollapsed ? "hidden" : "block"} h-8 w-auto`}/>
             </header>
         </div>
     )
