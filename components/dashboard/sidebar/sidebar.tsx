2052c37a5e7a03c2b82ac3b59157d94bcd36ef21
Timileyin Agbaje <timi.agbaje.mail@gmail.com>
2026-06-11 05:45:34 +0000

    fix: adjust sidebar width class for collapsed state and clean up unnecessary div

--- components/dashboard/sidebar/sidebar.tsx
+++ components/dashboard/sidebar/sidebar.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { SidebarProvider, useSidebar } from "@/components/dashboard/context/SidebarContext"
+import { useSidebar } from "@/components/dashboard/context/SidebarContext"
 import SidebarHeader from "@/components/dashboard/sidebar/sidebarHeader"
 import SidebarItems from "@/components/dashboard/sidebar/sidebarItems"
 import SidebarFooter from "@/components/dashboard/sidebar/sidebarFooter"
@@ -10,9 +10,9 @@ import { PanelRight } from 'lucide-react'
 export default function Sidebar( ) {
     const { isCollapsed, toggle } = useSidebar();
     return(
-           <aside className={`${isCollapsed ? "w-16" : "w-64"} flex flex-col h-screen left-0 space-y-2 bg-gray-50 border-r`}>
+           <aside className={`${isCollapsed ? "w-16 items-center-safe"  : "w-64"} flex flex-col h-screen left-0 space-y-2 bg-gray-50 border-r`}>
             <div className="flex flex-row justify-between p-2"><SidebarHeader /> <button onClick={toggle}><PanelRight className="cursor-pointer" /></button></div>
-            <div className="ml-4">
+            <div>
                 <SidebarItems />
                 <SidebarFooter /> 
             </div>
