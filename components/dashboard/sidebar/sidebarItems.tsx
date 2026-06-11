e740ffaa02405aedab15494d7b12493890a0b661
Timileyin Agbaje <timi.agbaje.mail@gmail.com>
2026-06-11 07:05:59 +0100

    Update components/dashboard/sidebar/sidebarItems.tsx
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- components/dashboard/sidebar/sidebarItems.tsx
+++ components/dashboard/sidebar/sidebarItems.tsx
@@ -5,7 +5,7 @@ export default function sidebarItems() {
     const { isCollapsed, toggle } = useSidebar();
     return(
         <div>
-            <section className={`${isCollapsed ? "gap-10" : "gap-1"}flex flex-col mt-10`}>
+            <section className={`${isCollapsed ? "gap-10" : "gap-1"} flex flex-col mt-10`}>
                 <button className=" hover:bg-gray-100  w-10/12 p-2 rounded-lg flex flex-row gap-1 items-center text-sm text-gray-800">
                 <MessageCircle className="w-6 h-6 shrink-0"/>
                 <p className={`${isCollapsed ? "hidden" : "block"}`}>New chat</p>
