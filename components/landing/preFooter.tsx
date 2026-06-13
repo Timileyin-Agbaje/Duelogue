5d95f5a46da951f448b2c56c712e3220bf665666
Timileyin Agbaje <timi.agbaje.mail@gmail.com>
2026-06-13 06:11:38 +0000

    Refactor footer, header, hero, and preFooter components to use Link for navigation

--- components/landing/preFooter.tsx
+++ components/landing/preFooter.tsx
@@ -1,11 +1,12 @@
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function PreFooter() {
     return(
         <section className="flex flex-col items-center">
             <h3 className="text-3xl font-bold">Ready To Compare?</h3>
             <p className="text-lg text-gray-600 mb-2">Type a topic. Discover Insights</p>
-            <Button asChild className="mb-16 mt-7"><a href="#features">Start Now</a></Button>
+            <Button asChild className="mb-16 mt-7"><Link href="/dashboard">Start Now</Link></Button>
         </section>
     )
 }
