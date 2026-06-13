475077c18a0c8b406028ae2554d8e9e875e34a7c
Timileyin Agbaje <timi.agbaje.mail@gmail.com>
2026-06-13 12:39:37 +0000

    Refactor Sidebar and SidebarItems components for improved UI consistency and accessibility

--- components/dashboard/sidebar/sidebarItems.tsx
+++ components/dashboard/sidebar/sidebarItems.tsx
@@ -5,12 +5,12 @@ export default function sidebarItems() {
     const { isCollapsed, toggle } = useSidebar();
     return(
         <div>
-            <section className={`${isCollapsed ? "gap-10" : "gap-1"} flex flex-col mt-10`}>
-                <button className=" hover:bg-gray-100  w-10/12 p-2 rounded-lg flex flex-row gap-1 items-center text-sm text-gray-800">
+            <section className={`${isCollapsed ? "gap-3" : "gap-1"} flex flex-col mt-10`}>
+                <button title="New Chat" className=" hover:bg-gray-100 focus:bg-gray-200  w-10/12 p-2 rounded-lg flex flex-row gap-1 items-center text-sm text-gray-800">
                 <MessageCircle className="w-6 h-6 shrink-0"/>
                 <p className={`${isCollapsed ? "hidden" : "block"}`}>New chat</p>
                 </button>
-                <button className="hover:bg-gray-100  w-10/12 p-2 rounded-lg flex flex-row gap-1 items-center text-sm  text-gray-800">
+                <button title="History" className="hover:bg-gray-100 focus:bg-gray-200  w-10/12 p-2 rounded-lg flex flex-row gap-1 items-center text-sm  text-gray-800">
                     <History className="w-6 h-6 shrink-0"/> <p className={`${isCollapsed ? "hidden" : "block"}`} >History</p>
                 </button>
             </section>
