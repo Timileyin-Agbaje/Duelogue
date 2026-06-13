f36fac5c8e3185e4ff47f4e18b2f07b74454223a
Timileyin Agbaje <timi.agbaje.mail@gmail.com>
2026-06-13 12:39:23 +0000

    Add minLength validation and aria-label to topic input for improved accessibility

--- components/dashboard/topicInput.tsx
+++ components/dashboard/topicInput.tsx
@@ -5,8 +5,8 @@ export default function topicInput() {
         <div className="relative w-full max-w-2xl">
             <form>
                 <section className='h-15 rounded-xl flex flex-row justify-between border p-2 shadow-lg'>
-                <input type="text" className=" w-11/12 appearance-none outline-none border-0 ring-0 focus:outline-none focus:ring-0"  placeholder="Enter a topic to see both sides..."></input>
-                <span className='w-10 h-10 bg-blue-600 rounded-full p-1 inline-flex items-center justify-center'><button type='submit'><ArrowUp color='#F9FAFB' /></button></span>
+                <input minLength={3} type="text" className=" w-11/12 appearance-none outline-none border-0 ring-0 focus:outline-none focus:ring-0"  placeholder="Enter a topic to see both sides..."></input>
+                <span className='w-10 h-10 bg-blue-600 rounded-full p-1 inline-flex items-center justify-center'><button type='submit' aria-label='Submit Topic'><ArrowUp color='#F9FAFB' /></button></span>
             </section>
             </form>
         </div>
