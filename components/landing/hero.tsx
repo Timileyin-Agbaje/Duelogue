670e1d4da31860240e3130839853dd7e846a434a
Timileyin <timi.agbaje.mail@gmail.com>
2026-06-27 14:41:21 +0100

    Update hero

--- components/landing/hero.tsx
+++ components/landing/hero.tsx
@@ -3,15 +3,31 @@ import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
 export default function Hero() {
-  return(
+  return (
     <section className="flex flex-col items-center">
-        <div className="px-4">
-            <h1 className="text-4xl sm:text-5xl font-extrabold md:text-7xl tracking-tight leading-tight mt-16 md:mt-20" >Discover Better Answers</h1>
-            <h2 className="text-xl md:text-2xl font-medium text-gray-600 max-w-3xl mx-auto text-center" >See both sides of any topic through clear comparison</h2>
-            <span className="flex justify-center gap-2 mt-4"><Button asChild className="cursor-pointer"><Link href="/dashboard">See pros & cons</Link></Button><Button asChild className="cursor-pointer" variant="secondary"><Link href="/how-it-works">How it works</Link></Button></span>
-            <Image src="/images/images.png" alt="Hero Image" width={800} height={600} className="w-full h-auto mt-8 object-contain"/>
-        </div>
+      <div className="px-4">
+        <h1 className="text-4xl sm:text-5xl font-extrabold md:text-7xl tracking-tight leading-tight mt-16 md:mt-20">
+          Discover Better Answers
+        </h1>
+        <h2 className="text-xl md:text-2xl font-medium text-gray-600 max-w-3xl mx-auto text-center">
+          See both sides of any topic through clear comparison
+        </h2>
+        <span className="flex justify-center gap-2 mt-4">
+          <Button asChild className="cursor-pointer">
+            <Link href="/dashboard">See pros & cons</Link>
+          </Button>
+          <Button asChild className="cursor-pointer" variant="secondary">
+            <Link href="/how-it-works">How it works</Link>
+          </Button>
+        </span>
+        <Image
+          src="/Image/Image.webp"
+          alt="Hero Image"
+          width={800}
+          height={600}
+          className="w-full h-auto mt-8 object-contain"
+        />
+      </div>
     </section>
-  )
-
+  );
 }
